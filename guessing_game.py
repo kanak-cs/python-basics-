ea7a9343d842fd1d414780090e6f8533e6e22e31
kanak-cs <sumitvikal26@gmail.com>
2026-09-20 12:41:51 +0530

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,28 +1,28 @@
-import random 
+import random
 
 def start_game():
-print("----------------------------------------")
-print("Welcome to Kanak's Number Guessing Games!")
-print("----------------------------------------")
-print("I'm thinking of a number between 1 and 20")
-
-# The computer picks a random secret number 
-secret_number = random.randint(1,20)
-attempts = 0
-
-while True:
-  # Get the user's guess 
-  guess = (int(input("Take a guess: "))
-  attempts +=1
-
-# Check if the guess is correct, too high, too low 
-if guess < secret_number:
-   print("Too low! Try guessing a higher number.")
-elif guess > secret_number:
-     print("Too high! Try guessing a lower number.")
-else:
-  print(f"Excellent job, Kanak! You guessed it in {attempts} attempts!")
-  break
+    print("-----------------------------------------")
+    print("Welcome to Kanak's Number Guessing Game!")
+    print("-----------------------------------------")
+    print("I am thinking of a number between 1 and 20.")
+    
+    # The computer picks a random secret number
+    secret_number = random.randint(1, 20)
+    attempts = 0
+    
+    while True:
+        # Get the user's guess
+        guess = int(input("Take a guess: "))
+        attempts += 1
+        
+        # Check if the guess is correct, too high, or too low
+        if guess < secret_number:
+            print("Too low! Try guessing a higher number.")
+        elif guess > secret_number:
+            print("Too high! Try guessing a lower number.")
+        else:
+            print(f"Excellent job, Kanak! You guessed it in {attempts} attempts!")
+            break
 
 # Start the game
-start_game() 
+start_game()
